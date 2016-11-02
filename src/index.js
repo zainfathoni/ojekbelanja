eab1c6f9d38b7049d57b478ab18188b27de95e27
Zain Fathoni <zain.fathoni@gmail.com>
2016-11-03 00:39:03 +0700

    add: BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import App from './App';
 import './css/index.css';
 import './css/fonts.css';
 
-ReactDOM.render(
-  <App />,
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={App} />
+        <Match pattern="/ojek/:ojekId" component={App} />
+        <Miss component={App} />        
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(
+  <Root />,
   document.getElementById('root')
 );
