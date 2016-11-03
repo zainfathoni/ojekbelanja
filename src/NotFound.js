68ec52265d79905fa49bdefe216b596fd2c4fb74
Zain Fathoni <zain.fathoni@gmail.com>
2016-11-03 17:36:44 +0700

    refactor: Class Names

--- src/NotFound.js
+++ src/NotFound.js
@@ -7,16 +7,16 @@ import './css/NotFound.css';
 class NotFound extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <Navigation />
-        <div className="App-header">
+        <div className="app-header">
           <h1>Ojek <i className="material-icons">motorcycle</i><i className="material-icons">loop</i> Nyasar</h1>
-          <h3 className="header-error">404 | Page Not Found</h3>
+          <h3 className="app-header-error">404 | Page Not Found</h3>
         </div>
-        <p className="App-intro">
+        <p className="app-intro">
           Maaf, halaman yang Anda tuju tidak ditemukan.
         </p>
-        <p className="App-intro">
+        <p className="app-intro">
           Silakan kembali ke <Link to="/">halaman utama</Link>.
         </p>
       </div>
