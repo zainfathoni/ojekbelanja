71a65300b9588645267dc44e987fa298f7cc24b3
Zain Fathoni <zain.fathoni@gmail.com>
2016-11-03 14:52:10 +0700

    edit: Element Selectors into Class Selectors

--- src/Navigation.js
+++ src/Navigation.js
@@ -6,13 +6,13 @@ import './css/Navigation.css';
 class Navigation extends Component {
   render() {
     return (
-      <header>
-        <nav>
-          <Link to="/" className="home">Ojek <i className="material-icons">motorcycle</i><i className="material-icons">shopping_basket</i> Belanja</Link>
-          <Link to="/cara-kerja" activeClassName="active">Cara Kerja</Link>
-          <Link to="/faq" activeClassName="active">FAQ</Link>
-          <Link to="/tentang-kami" activeClassName="active">Tentang Kami</Link>
-          <Link to="/login" activeClassName="active"><i className="material-icons">exit_to_app</i> Login</Link>
+      <header className="nav-container">
+        <nav className="nav-wrap">
+          <Link to="/" className="nav nav-home">Ojek <i className="material-icons">motorcycle</i><i className="material-icons">shopping_basket</i> Belanja</Link>
+          <Link to="/cara-kerja" className="nav nav-item" activeClassName="is-active">Cara Kerja</Link>
+          <Link to="/faq" className="nav nav-item" activeClassName="is-active">FAQ</Link>
+          <Link to="/tentang-kami" className="nav nav-item" activeClassName="is-active">Tentang Kami</Link>
+          <Link to="/login" className="nav nav-login" activeClassName="is-active"><i className="material-icons">exit_to_app</i> Login</Link>
         </nav>
       </header>
     );
