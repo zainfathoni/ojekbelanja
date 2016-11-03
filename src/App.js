defd1733e8cd14612f2fcad2c8218fd51c5fadb2
Zain Fathoni <zain.fathoni@gmail.com>
2016-11-03 08:33:16 +0700

    edit: Remove logo.svg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import Navigation from './Navigation';
-import logo from './css/images/logo.svg';
 import './css/App.css';
 
 class App extends Component {
@@ -16,7 +15,6 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Ojek <i className="material-icons">motorcycle</i><i className="material-icons">shopping_basket</i> Belanja</h2>
         </div>
         <p className="App-intro">
