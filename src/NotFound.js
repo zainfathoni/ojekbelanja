dda6666a34eb88d3d010253b7b04962b048e73ab
Zain Fathoni <zain.fathoni@gmail.com>
2016-11-03 16:24:17 +0700

    edit: NotFound Page

--- src/NotFound.js
+++ src/NotFound.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 import Navigation from './Navigation';
-import './css/App.css';
+import './css/NotFound.css';
 
 class NotFound extends Component {
   render() {
@@ -10,7 +10,16 @@ class NotFound extends Component {
       <div className="App">
         <Navigation />
         <div className="App-header">
-          <h2>Ojek <i className="material-icons">motorcycle</i><i className="material-icons">loop</i> Nyasar</h2>
+          <h2>
+            <span className="header-404">
+              404 Error
+            </span>
+            <br />
+            <span className="header-ojek">
+              Ojek <i className="material-icons">motorcycle</i>
+              <i className="material-icons">loop</i> Nyasar
+            </span>
+          </h2>
         </div>
         <p className="App-intro">
           Maaf, halaman yang Anda tuju tidak ditemukan.
