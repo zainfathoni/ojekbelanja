29abcfe424e55782bf5d41bcda90f59bac8f760f
Zain Fathoni <zain.fathoni@gmail.com>
2016-11-06 08:23:44 +0700

    add: Styling for FilterInput component without Button element

--- src/components/FilterInput/index.js
+++ src/components/FilterInput/index.js
@@ -8,14 +8,16 @@ export default function FilterInput(props) {
       <input
         type="text"
         className={"filter-input filter-input-txt" +
-          (props.withButton ? "" : " filter-input-txt-no-button")}
+          (props.withButton ? "" : " filter-input-txt-no-btn")}
         placeholder={props.placeholder}
         value={props.keyword}
         onChange={(e) => props.action(e.target.value)}
         />
-      <button className="filter-input filter-input-btn">
-        <i className="material-icons">search</i>
-      </button>
+      {props.withButton &&
+        <button className="filter-input filter-input-btn">
+          <i className="material-icons">search</i>
+        </button>
+      }
     </div>
   )
 }
--- src/components/TokoPicker/index.js
+++ src/components/TokoPicker/index.js
@@ -36,7 +36,6 @@ export default class TokoPicker extends Component {
           placeholder="Cari Nama atau Area Layanan"
           keyword={this.state.keyword}
           action={this.updateKeyword}
-          withButton
           />
         <FilterCards
           keyword={this.state.keyword}
