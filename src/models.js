20403c9293bcfc19607f75a87479ef80032a2526
Zain Fathoni <zain.fathoni@gmail.com>
2016-11-07 23:15:37 +0700

    add: products & categories in sample models

--- src/models.js
+++ src/models.js
@@ -26,32 +26,176 @@ export var tokos = {
 }
 
 export var products = {
-  pisang: {
-    name: 'Pisang Ambon',
-    desc: 'Pisang lezat dari Ambon',
+  bawang_putih: {
+    name: 'Bawang Putih',
+    desc: 'Bawang Putih',
     image: 'placeholder-224x224.png',
-    unit: 'buah',
-    price: '3000'
+    unit: 'kg',
+    step: .25,
+    price: 35000,
+    category: 'bumbu'
   },
-  kecap: {
-    name: 'Kecap Ambon',
-    desc: 'Kecap lezat dari Ambon',
+  bawang_merah: {
+    desc: 'Bawang Merah',
+    name: 'Bawang Merah',
     image: 'placeholder-224x224.png',
-    unit: 'buah',
-    price: '3000'
+    unit: 'kg',
+    step: .25,
+    price: 40000,
+    category: 'bumbu'
   },
-  ikan: {
-    name: 'Ikan Ambon',
-    desc: 'Ikan lezat dari Ambon',
+  bawang_bombay: {
+    desc: 'Bawang Bombay',
+    name: 'Bawang Bombay',
     image: 'placeholder-224x224.png',
-    unit: 'buah',
-    price: '3000'
+    unit: 'kg',
+    step: .25,
+    price: 22000,
+    category: 'bumbu'
+  },
+  cabe_gendot: {
+    desc: 'Cabe Gendot',
+    name: 'Cabe Gendot',
+    image: 'placeholder-224x224.png',
+    unit: 'kg',
+    step: .25,
+    price: 35000,
+    category: 'bumbu'
+  },
+  cabe_tanjung: {
+    desc: 'Cabe Tanjung',
+    name: 'Cabe Tanjung',
+    image: 'placeholder-224x224.png',
+    unit: 'kg',
+    step: .25,
+    price: 47000,
+    category: 'bumbu'
+  },
+  cabe_keriting: {
+    desc: 'Cabe Keriting',
+    name: 'Cabe Keriting',
+    image: 'placeholder-224x224.png',
+    unit: 'kg',
+    step: .25,
+    price: 48000,
+    category: 'bumbu'
+  },
+  cengek: {
+    name: 'Cengek',
+    desc: 'Cengek',
+    image: 'placeholder-224x224.png',
+    unit: 'kg',
+    step: .25,
+    price: 46000,
+    category: 'bumbu'
+  },
+  cengek_hijau: {
+    desc: 'Cengek Hijau',
+    name: 'Cengek Hijau',
+    image: 'placeholder-224x224.png',
+    unit: 'kg',
+    step: .25,
+    price: 18000,
+    category: 'bumbu'
+  },
+  bawang_daun: {
+    desc: 'Bawang Daun',
+    name: 'Bawang Daun',
+    image: 'placeholder-224x224.png',
+    unit: 'kg',
+    step: .25,
+    price: 24000,
+    category: 'bumbu'
+  },
+  jahe: {
+    desc: 'Jahe',
+    name: 'Jahe',
+    image: 'placeholder-224x224.png',
+    unit: 'kg',
+    step: .25,
+    price: 16000,
+    category: 'bumbu'
   },
   ayam: {
-    name: 'Ayam Ambon',
-    desc: 'Ayam lezat dari Ambon',
+    desc: 'Ayam',
+    name: 'Ayam',
     image: 'placeholder-224x224.png',
-    unit: 'buah',
-    price: '3000'
+    unit: 'kg',
+    step: .25,
+    price: 32000,
+    category: 'ayam'
+  },
+  ayam_fillet: {
+    desc: 'Ayam Fillet',
+    name: 'Ayam Fillet',
+    image: 'placeholder-224x224.png',
+    unit: 'kg',
+    step: .25,
+    price: 42000,
+    category: 'ayam'
+  },
+  kepala_ayam: {
+    name: 'Kepala Ayam',
+    desc: 'Kepala Ayam',
+    image: 'placeholder-224x224.png',
+    unit: 'kg',
+    step: .25,
+    price: 10000,
+    category: 'ayam'
+  },
+  usus_ayam: {
+    desc: 'Usus Ayam',
+    name: 'Usus Ayam',
+    image: 'placeholder-224x224.png',
+    unit: 'kg',
+    step: .25,
+    price: 16000,
+    category: 'ayam'
+  },
+  ceker_ayam: {
+    desc: 'Ceker Ayam',
+    name: 'Ceker Ayam',
+    image: 'placeholder-224x224.png',
+    unit: 'kg',
+    step: .25,
+    price: 20000,
+    category: 'ayam'
+  },
+  sayap_ayam: {
+    desc: 'Sayap Ayam',
+    name: 'Sayap Ayam',
+    image: 'placeholder-224x224.png',
+    unit: 'kg',
+    step: .25,
+    price: 30000,
+    category: 'ayam'
+  },
+  ati_ampela: {
+    desc: 'Ati Ampela',
+    name: 'Ati Ampela 10 Pasang',
+    image: 'placeholder-224x224.png',
+    unit: 'pasang',
+    step: 10,
+    price: 22000,
+    category: 'ayam'
+  },
+  telur_ayam: {
+    desc: 'Telur Ayam',
+    name: 'Telur Ayam',
+    image: 'placeholder-224x224.png',
+    unit: 'kg',
+    step: .25,
+    price: 19500,
+    category: 'ayam'
   }
+}
+
+export var categories = {
+  bumbu: 'Bumbu',
+  sayuran: 'Sayuran',
+  ikan: 'Ikan',
+  ayam: 'Daging Ayam',
+  sapi: 'Daging Sapi',
+  cumi: 'Cumi',
+  udang: 'Udang'
 }
