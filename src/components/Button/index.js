a21833bd1afee210f5adba9e4db303c65e5f988b
Zain Fathoni <zain.fathoni@gmail.com>
2016-11-13 11:55:04 +0700

    add: #20 classnames usages in Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,19 @@
 import React, { PropTypes as T } from 'react';
+import classNames from 'classnames';
 
 import './Button.css';
 
 export default function Button(props) {
+  const {
+    type
+  } = props;
+  const buttonClass = classNames(
+    'Button',
+    `Button-${type}`
+  )
+  
   return (
-    <button className="Button" onClick={props.action}>
+    <button className={buttonClass} onClick={props.action}>
       <i className={`fa fa-lg fa-${props.icon}`} aria-hidden="true"></i>
       {` ${props.text}`}
     </button>
@@ -18,12 +27,12 @@ Button.defaultProps = {
 }
 
 Button.propTypes = {
-  type: T.oneOf([ // Button Type
+  type: T.oneOf([
     'fullwidth',
     'large',
     'small',
     'icon'
-  ]).isRequired,
+  ]).isRequired, // Button Type
   action: T.func.isRequired, // Action Function
   icon: T.string, // Icon ID in Font Awesome
   text: T.string // Button Text
