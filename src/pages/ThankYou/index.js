42779f12b1284f69d6ccfe267d97d7bba2b6e0c0
Zain Fathoni <zain.fathoni@gmail.com>
2016-11-17 11:29:15 +0700

    add: #23 ThankYou description styling

--- src/pages/ThankYou/index.js
+++ src/pages/ThankYou/index.js
@@ -18,10 +18,24 @@ export default class NotFound extends Component {
           <MainNav />
         </div>
         <Header heading={"Terima kasih telah berbelanja di Toko " + tokos[tokoId].name} />
-        <p>Hai Zain, berikut detil transaksi Anda:</p>
         <main className="l-ThankYou">
+          <p>Hai Zain, berikut detil transaksi Anda:</p>
           <InfoPemesan />
           <InfoPesanan />
+          <h4>Cara Pembayaran</h4>
+          <p>
+            <ol>
+              <li>Make sure you have received the order confirmation email from SayurBox.</li>
+              <li>Transfer the total transaction amount with reference to your order number to SayurBox BCA account with the following details:
+              <ul>
+                  <li><b>Account Name : PT. KREASI NOSTRA MANDIRI</b></li>
+                  <li><b>Account No : 006 – 9197771</b></li>
+                </ul>
+              </li>
+              <li>Please make payment by :<div><b>Monday 5 p.m</b> for Wednesday’s delivery, and <b>Thursday by 5 p.m</b> for Saturday’s delivery to avoid order cancellation.</div>Once you have paid, you will receive a whatsapp or email confirmation.
+              </li>
+            </ol>
+          </p>
         </main>
       </div>
     );
