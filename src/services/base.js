fa703cf522204f47a24893c22dfc256c022792c7
Zain Fathoni <zain.fathoni@gmail.com>
2017-07-29 21:13:31 +0700

    upgrade: re-base add: firebase

--- src/services/base.js
+++ src/services/base.js
@@ -1,11 +1,13 @@
 import Rebase from 're-base';
+import firebase from 'firebase';
 
-const base = Rebase.createClass({
+const app = firebase.initializeApp({
   apiKey: "AIzaSyAxEtU2NTT51cYCtXEhneq6xBqbaU5rcFU",
   authDomain: "ojekbelanja.firebaseapp.com",
   databaseURL: "https://ojekbelanja.firebaseio.com",
   storageBucket: "ojekbelanja.appspot.com",
   messagingSenderId: "952122011257",
-})
+});
 
+const base = Rebase.createClass(app.database());
 export default base;
