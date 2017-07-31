22721022cd66acef2d57d0522adda02ca6b2ebef
Zain Fathoni <zain.fathoni@gmail.com>
2017-07-31 20:21:44 +0700

    refactor: Replace models in Pesan with state fetched from Firebase

--- src/pages/Pesan/index.js
+++ src/pages/Pesan/index.js
@@ -1,52 +1,67 @@
 import React from 'react';
+import { PropTypes as T } from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import { getStore } from '../../reducers';
 import MainNav from '../../components/MainNav';
 import Header from '../../components/Header';
 import Pesanan from './Pesanan';
 import PemesanContainer from '../../containers/PemesanContainer';
-import { stores } from '../../models';
 import '../pages.css';
 import './Pesan.css';
 
-let Pesan = ({ match, order }) => {
-  const storeId = match.params.storeId;
+let Pesan = ({ id, toko, order }) => {
 
   return (
-    !order || Object.keys(order).length === 0 ? (
+    !toko || !order || Object.keys(order).length === 0 ? (
       // No ordered Item, go back to Toko page
-      <Redirect to={`/toko/${storeId}`}/>
+      <Redirect to={`/toko/${id}`}/>
     ) : (
       <div className="l-fullwidth">
         <div className="l-wrapper-MainNav">
           <MainNav />
         </div>
-        <Header heading={"Toko " + stores[storeId].name} />
+        <Header heading={"Toko " + toko.name} />
         <main className="l-main">
           <div className="l-Pesan">
             <Pesanan
               name="order"
               order={order}
-              storeId={storeId}
+              storeId={id}
               />
           </div>
           <div className="l-Pesan">
             <PemesanContainer
               name="user"
-              storeId={storeId}
+              storeId={id}
               />
           </div>
-
         </main>
       </div>
     )
   )
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  order: state.order,
-});
+Pesan.propTypes = {
+  id: T.string.isRequired,
+  toko: T.shape({
+    name: T.string.isRequired,
+    area: T.string.isRequired,
+    image: T.string.isRequired,
+    cost: T.number.isRequired,
+  }),
+  order: T.objectOf(T.number).isRequired,
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.storeId;
+  return {
+    id,
+    toko: getStore(state, id),
+    order: state.order,
+  }
+};
 
 Pesan = connect(
   mapStateToProps,
