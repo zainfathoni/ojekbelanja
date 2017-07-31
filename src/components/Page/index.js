d3c0a50779efe3aae3fcc9be6ed51c056e2fb29f
Zain Fathoni <zain.fathoni@gmail.com>
2017-08-01 00:29:23 +0700

    add: Page Component

---
+++ src/components/Page/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { PropTypes as T } from 'prop-types';
+
+import MainNav from '../MainNav';
+import Header from '../Header';
+import './Page.css';
+
+const Page = ({
+  header,
+  children,
+  footer,
+}) => (
+  <div className="l-fullwidth">
+    <div className="l-wrapper-MainNav">
+      <MainNav />
+    </div>
+    <div className="l-wrapper-header">
+      {header}
+    </div>
+    <main className="l-main">
+      {children}
+    </main>
+    {footer &&
+      <footer className="l-wrapper-footer">
+        {footer}
+      </footer>
+    }
+  </div>
+)
+
+Page.defaultProps = {
+  header: <Header />
+}
+
+Page.propTypes = {
+  header: T.element.isRequired,
+  children: T.element,
+  footer: T.element,
+}
+
+export default Page;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
 
-import MainNav from '../../components/MainNav';
-import Header from '../../components/Header';
+import Page from '../../components/Page';
 import Tokos from '../../containers/Tokos';
 import '../pages.css';
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="l-fullwidth">
-        <div className="l-wrapper-MainNav">
-          <MainNav />
+      <Page>
+        <div>
+          <p>
+            Pilih Toko <strong>Ojek Belanja</strong> Anda
+          </p>
+          <Tokos />
         </div>
-        <Header />
-        <p>
-          Pilih Toko <strong>Ojek Belanja</strong> Anda
-        </p>
-        <Tokos />
-      </div>
+      </Page>
     );
   }
 }
