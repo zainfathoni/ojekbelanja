78c5b993f3683e13db84b757c49494fb7c5f416b
Zain Fathoni <zain.fathoni@gmail.com>
2017-07-31 23:25:41 +0700

    remove: products.total service

--- src/services/product.js
+++ src/services/product.js
@@ -1,12 +1,3 @@
 export const escapeFloatingPoint = (value) =>
   // Avoid Floating Point Problem
   Math.round(value * 100) / 100;
-
-export const total = (order, products) =>
-  Object.keys(order)
-    .reduce(
-    (sum, key) =>
-      sum + (products[key].price * products[key].step * order[key])
-    ,
-    0
-    );
