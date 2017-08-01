be770f47822cc45fedc64fc1f20afa5f77202fbd
Zain Fathoni <zain.fathoni@gmail.com>
2017-08-01 21:35:02 +0700

    refactor: NotFound Page to use Page Component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Page from '../../components/Page';
 import Tokos from '../../containers/Tokos';
-import '../pages.css';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <Page>
-        <div>
-          <p>
-            Pilih Toko <strong>Ojek Belanja</strong> Anda
-          </p>
-          <Tokos />
-        </div>
-      </Page>
-    );
-  }
-}
+const Home = () => (
+  <Page>
+    <div>
+      <p>
+        Pilih Toko <strong>Ojek Belanja</strong> Anda
+      </p>
+      <Tokos />
+    </div>
+  </Page>
+);
+
+export default Home;
--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import MainNav from '../../components/MainNav';
+import Page from '../../components/Page';
 import Header from '../../components/Header';
-import '../pages.css';
 
-export default function NotFound(props) {
-  return (
-    <div className="l-fullwidth">
-      <div className="l-wrapper-MainNav">
-        <MainNav />
-      </div>
-      <Header is404={true} />
+const NotFound = () => (
+  <Page
+    header={<Header is404={true} />}
+  >
+    <div>
       <p>
         Maaf, halaman yang Anda tuju tidak ditemukan.
-        </p>
+      </p>
       <p>
         Silakan kembali ke <Link to="/">halaman utama</Link>.
-        </p>
+      </p>
     </div>
-  );
-}
+  </Page>
+);
+
+export default NotFound;
