6b193d557b37bb0d9534012c8ae8e18996863fa9
Zain Fathoni <zain.fathoni@gmail.com>
2017-09-24 07:47:43 +0700

    add: @storybook/react

---
+++ src/components/Brand/Brand.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Brand from "./index";
+
+storiesOf("Brand", module).add("default", () => <Brand />);
---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Button from "./index";
+
+storiesOf("Button", module)
+  .addDecorator(story => <div style={{ width: 200 }}>{story()}</div>)
+  .add("default", () => <Button />)
+  .add("primary", () => (
+    <Button display="fullwidth" icon="shopping-cart" text="Fullwidth" />
+  ))
+  .add("secondary", () => (
+    <Button display="content" icon="shopping-cart" text="Content" isSecondary />
+  ));
