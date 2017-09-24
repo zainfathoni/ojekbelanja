85e4fa332657df0f2d33aa49355758d8f19d46c4
Zain Fathoni <zain.fathoni@gmail.com>
2017-09-24 09:21:10 +0700

    add: Notes addon

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,26 +1,44 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
 import { withKnobs, text, boolean, select } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import { withNotes } from "@storybook/addon-notes";
 import Button from "./index";
 
 storiesOf("Button", module)
   .addDecorator(story => <div style={{ width: 200 }}>{story()}</div>)
   .addDecorator(withKnobs)
-  .add("text", () => (
-    <Button
-      action={action("button-click")}
-      icon="shopping-cart"
-      display={select(
-        "Display Mode",
-        ["fullwidth", "content", "icon"],
-        "fullwidth"
-      )}
-      text={text("Text", "Button")}
-      isSecondary={boolean("isSecondary", false)}
-      disabled={boolean("Disabled", false)}
-    />
-  ))
-  .add("icon", () => (
-    <Button action={action("button-click")} display="icon" icon="trash" />
-  ));
+  .add(
+    "text",
+    withNotes("Button with icon and text")(() => (
+      <Button
+        action={action("button-click")}
+        icon="shopping-cart"
+        display={select(
+          "Display Mode",
+          ["fullwidth", "content", "icon"],
+          "fullwidth"
+        )}
+        text={text("Text", "Beli")}
+        isSecondary={boolean("isSecondary", false)}
+        disabled={boolean("Disabled", false)}
+      />
+    ))
+  )
+  .add(
+    "icon",
+    withNotes("Button with icon only")(() => (
+      <Button
+        action={action("button-click")}
+        icon="trash"
+        display={select(
+          "Display Mode",
+          ["fullwidth", "content", "icon"],
+          "icon"
+        )}
+        text={text("Text", "Hapus")}
+        isSecondary={boolean("isSecondary", true)}
+        disabled={boolean("Disabled", true)}
+      />
+    ))
+  );
