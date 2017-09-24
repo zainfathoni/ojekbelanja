fb1cae3390f55039356d05e6d4e628e2d50d9ab0
Zain Fathoni <zain.fathoni@gmail.com>
2017-09-24 08:22:23 +0700

    add: Actions addon

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import Button from "./index";
 
 storiesOf("Button", module)
   .addDecorator(story => <div style={{ width: 200 }}>{story()}</div>)
-  .add("default", () => <Button />)
+  .add("default", () => <Button action={action("button-click")} />)
   .add("primary", () => (
-    <Button display="fullwidth" icon="shopping-cart" text="Fullwidth" />
+    <Button
+      action={action("button-click")}
+      display="fullwidth"
+      icon="shopping-cart"
+      text="Fullwidth"
+    />
   ))
   .add("secondary", () => (
-    <Button display="content" icon="shopping-cart" text="Content" isSecondary />
+    <Button
+      action={action("button-click")}
+      display="content"
+      icon="shopping-cart"
+      text="Content"
+      isSecondary
+    />
   ))
-  .add("icon", () => <Button display="icon" icon="trash" text="Hapus" />);
+  .add("icon", () => (
+    <Button action={action("button-click")} display="icon" icon="trash" />
+  ));
