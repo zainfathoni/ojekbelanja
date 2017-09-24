91defc5c51669e79ae27ab36c2dd09a43079ec79
Zain Fathoni <zain.fathoni@gmail.com>
2017-09-24 09:33:31 +0700

    add: JSX addons

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,41 +1,42 @@
 import React from "react";
-import { storiesOf } from "@storybook/react";
+import { setAddon, storiesOf } from "@storybook/react";
 import { withKnobs, text, boolean, select } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
 import { withNotes } from "@storybook/addon-notes";
+import JSXAddon from "storybook-addon-jsx";
 import Button from "./index";
+
+setAddon(JSXAddon);
+
+const options = {
+  fullwidth: "Fullwidth",
+  content: "Content",
+  icon: "Icon"
+};
 
 storiesOf("Button", module)
   .addDecorator(story => <div style={{ width: 200 }}>{story()}</div>)
   .addDecorator(withKnobs)
-  .add(
+  .addWithJSX(
     "text",
     withNotes("Button with icon and text")(() => (
       <Button
         action={action("button-click")}
-        icon="shopping-cart"
-        display={select(
-          "Display Mode",
-          ["fullwidth", "content", "icon"],
-          "fullwidth"
-        )}
+        display={select("Display Mode", options, "fullwidth")}
+        icon={text("Icon", "shopping-cart")}
         text={text("Text", "Beli")}
         isSecondary={boolean("isSecondary", false)}
         disabled={boolean("Disabled", false)}
       />
     ))
   )
-  .add(
+  .addWithJSX(
     "icon",
     withNotes("Button with icon only")(() => (
       <Button
         action={action("button-click")}
-        icon="trash"
-        display={select(
-          "Display Mode",
-          ["fullwidth", "content", "icon"],
-          "icon"
-        )}
+        display={select("Display Mode", options, "icon")}
+        icon={text("Icon", "trash")}
         text={text("Text", "Hapus")}
         isSecondary={boolean("isSecondary", true)}
         disabled={boolean("Disabled", true)}
