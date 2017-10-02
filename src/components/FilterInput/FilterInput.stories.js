35051f298ef4168542cadbbba4fe023ea99ebbfa
Zain Fathoni <zain.fathoni@gmail.com>
2017-10-03 02:26:48 +0700

    add: FilterInput stories

---
+++ src/components/FilterInput/FilterInput.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { setAddon, storiesOf } from "@storybook/react";
+import { withKnobs, text, boolean } from "@storybook/addon-knobs";
+import JSXAddon from "storybook-addon-jsx";
+import { withNotes } from "@storybook/addon-notes";
+import { action } from "@storybook/addon-actions";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import FilterInput from "./index";
+
+setAddon(JSXAddon);
+
+storiesOf("FilterInput", module)
+  .addDecorator(story => (
+    <ul style={{ width: 200, textAlign: "center" }}>{story()}</ul>
+  ))
+  .addDecorator(withKnobs)
+  .addWithJSX(
+    "with Button",
+    withNotes("Filter Input with Button")(() => (
+      <FilterInput
+        placeholder={text("Placeholder", "Cari Produk")}
+        keyword={text("Keyword", "ay")}
+        withButton={boolean("with Button", true)}
+        setKeyword={action("set-keyword")}
+      />
+    ))
+  )
+  .addWithJSX(
+    "without Button",
+    withNotes("Filter Input with Button")(() => (
+      <FilterInput
+        placeholder={text("Placeholder", "Cari Nama atau Area Layanan")}
+        keyword={text("Keyword", "")}
+        withButton={boolean("with Button", false)}
+        setKeyword={action("set-keyword")}
+      />
+    ))
+  );
