f542365b3b95b55e5784847b2f35c24a06fd8b85
Zain Fathoni <zain.fathoni@gmail.com>
2017-10-03 13:46:24 +0700

    add: FilterInput test assertions

--- src/components/FilterInput/FilterInput.test.js
+++ src/components/FilterInput/FilterInput.test.js
@@ -1,34 +1,45 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import FilterInput from "./index";
 
 require("../../../.storybook/enzyme_setup.js");
 
 describe("FilterInput", () => {
   it("FilterInput with Button behaves without error", () => {
-    const filterInput = shallow(
+    let keyword;
+    const filterInput = mount(
       <FilterInput
         placeholder="Cari Produk"
         keyword="ay"
         withButton
-        setKeyword={keyword => console.log(keyword)}
+        setKeyword={key => {
+          keyword = key;
+        }}
       />
     );
     const input = filterInput.find("input");
-    input.simulate("change", { target: { value: "ayay" } });
-    const form = filterInput.find("form").first();
+    input.simulate("change", { target: { value: "ayam" } });
+    expect(keyword).toBeUndefined;
+
+    const form = filterInput.find("form");
     form.simulate("submit", { preventDefault: () => {} });
+    expect(keyword).toBe("ayam");
   });
 
   it("FilterInput without Button behaves without error", () => {
-    const filterInput = shallow(
+    let keyword;
+    const filterInput = mount(
       <FilterInput
         placeholder="Cari Produk"
         keyword="ay"
-        setKeyword={keyword => console.log(keyword)}
+        setKeyword={key => {
+          keyword = key;
+        }}
       />
     );
+    expect(keyword).toBeUndefined;
     const input = filterInput.find("input");
-    input.simulate("change", { target: { value: "ayay" } });
+    input.simulate("change", { target: { value: "ayam" } });
+    expect(keyword).toBe("ayam");
   });
 });
