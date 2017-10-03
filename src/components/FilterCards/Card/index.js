c3ebcced0d7a35d1c8bd1c4d466b1e2120ea2ef4
Zain Fathoni <zain.fathoni@gmail.com>
2017-10-03 13:02:55 +0700

    refactor: action & actionReverse props

--- src/components/FilterCards/Card/index.js
+++ src/components/FilterCards/Card/index.js
@@ -71,7 +71,7 @@ export default function Card(props) {
               {disabled ? (
                 <Button
                   display="fullwidth"
-                  action={action}
+                  action={() => action(id)}
                   icon="ban"
                   text="Stok Habis"
                   disabled
@@ -83,7 +83,7 @@ export default function Card(props) {
                       <Button
                         className="Card-action-minus"
                         display="icon"
-                        action={actionReverse}
+                        action={() => actionReverse(id)}
                         icon="minus"
                         text="Kurangi"
                         isSecondary
@@ -91,7 +91,7 @@ export default function Card(props) {
                       <Button
                         className="Card-action-plus"
                         display="icon"
-                        action={action}
+                        action={() => action(id)}
                         icon="plus"
                         text="Tambahkan"
                       />
@@ -99,7 +99,7 @@ export default function Card(props) {
                   ) : (
                     <Button
                       display="fullwidth"
-                      action={action}
+                      action={() => action(id)}
                       icon="cart-plus"
                       text="Beli"
                     />
