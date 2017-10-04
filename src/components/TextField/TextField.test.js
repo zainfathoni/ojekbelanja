4380a47707100cc22566b61f2402642b116af4eb
Zain Fathoni <zain.fathoni@gmail.com>
2017-10-04 09:19:42 +0700

    add: TextField behavior tests

---
+++ src/components/TextField/TextField.test.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { mount } from "enzyme";
+
+import { isEmailValid } from "../../services/form";
+import TextField from ".";
+
+require("../../../.storybook/enzyme_setup.js");
+
+describe("TextField", () => {
+  let textField, email, field, value;
+  beforeEach(() => {
+    textField = mount(
+      <TextField
+        type="email"
+        name="email"
+        label="Email"
+        value=""
+        onChange={(f, v) => {
+          field = f;
+          value = v;
+        }}
+        validate={isEmailValid}
+        message="Alamat Email tidak valid"
+        required
+      />
+    );
+    email = textField.find("#email");
+    field = undefined;
+    value = undefined;
+  });
+
+  it(" alls onChange correctly", () => {
+    email.simulate("change", { target: { value: "gmail" } });
+    expect(field).toBe("email");
+    expect(value).toBe("gmail");
+  });
+
+  it("shows Requiring Message correctly", () => {
+    expect(email.text()).toEqual("");
+    const message = textField.find(".TextField");
+    expect(message.text()).toEqual("Email");
+
+    email.simulate("focus");
+    email.simulate("blur");
+
+    expect(message.text()).toEqual("Email* Email harus diisi");
+  });
+
+  it("shows Validation Message correctly", () => {
+    textField.setProps({ value: "gmail" });
+    expect(email.text()).toEqual("");
+    const message = textField.find(".TextField");
+    expect(message.text()).toEqual("Email");
+
+    email.simulate("focus");
+    email.simulate("blur");
+
+    expect(message.text()).toEqual("Email* Alamat Email tidak valid");
+  });
+
+  it("valid by default", () => {
+    const defaultTextField = mount(
+      <TextField
+        name="email"
+        label="Email"
+        value=""
+        onChange={(f, v) => {
+          field = f;
+          value = v;
+        }}
+      />
+    );
+    email = defaultTextField.find("#email");
+    expect(email.text()).toEqual("");
+    const message = textField.find(".TextField");
+    expect(message.text()).toEqual("Email");
+
+    email.simulate("focus");
+    email.simulate("blur");
+
+    expect(message.text()).toEqual("Email");
+  });
+});
