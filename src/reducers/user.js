5f7a34ea5b7c9235cbe9fb91f0b5c10ccc4a34d7
Zain Fathoni <zain.fathoni@gmail.com>
2017-10-05 05:51:28 +0700

    add: More user selectors

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -15,7 +15,6 @@ export default user;
 
 export const isValid = (state, field) => {
   const value = state[field];
-  console.log(value);
   switch (field) {
     case "email":
       return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
@@ -28,4 +27,22 @@ export const isValid = (state, field) => {
     default:
       return true;
   }
+};
+
+export const isUserValid = state => {
+  return Object.keys(state).filter(field => !isValid(state, field)).length == 0;
+};
+
+const requiredFields = ["name", "email", "phone", "city", "address"];
+
+export const isRequired = field => {
+  return requiredFields.includes(field);
+};
+
+export const isRequirementFulfilled = (state, field) => {
+  return isRequired(field) ? state[field] : true;
+};
+
+export const areRequirementsFulfilled = state => {
+  return requiredFields.filter(field => !state[field]).length == 0;
 };
--- src/reducers/user.test.js
+++ src/reducers/user.test.js
@@ -1,4 +1,10 @@
-import user, { isValid } from "./user";
+import user, {
+  isValid,
+  isUserValid,
+  isRequired,
+  isRequirementFulfilled,
+  areRequirementsFulfilled
+} from "./user";
 import { setUser, clearUser } from "../actions";
 
 test("SET_USER", () => {
@@ -46,5 +52,74 @@ describe("isValid", () => {
   });
   it("default", () => {
     expect(isValid({ password: "12345" }, "pass")).toBe(true);
+  });
+});
+
+describe("isUserValid", () => {
+  it("yes", () => {
+    expect(
+      isUserValid({
+        email: "zainfathoni@ojekbelanja.id",
+        phone: "081234567890",
+        password: "123456"
+      })
+    ).toBe(true);
+  });
+});
+
+describe("isRequired", () => {
+  it("name", () => {
+    expect(isRequired("name")).toBe(true);
+  });
+  it("email", () => {
+    expect(isRequired("email")).toBe(true);
+  });
+  it("phone", () => {
+    expect(isRequired("phone")).toBe(true);
+  });
+  it("city", () => {
+    expect(isRequired("city")).toBe(true);
+  });
+  it("address", () => {
+    expect(isRequired("address")).toBe(true);
+  });
+  it("password", () => {
+    expect(isRequired("password")).toBe(false);
+  });
+});
+
+describe("isRequirementFulfilled", () => {
+  it("string yes", () => {
+    expect(isRequirementFulfilled({ name: "Zain" }, "name")).toBeTruthy;
+  });
+  it("string no", () => {
+    expect(isRequirementFulfilled({ name: "" }, "name")).toBeFalsy;
+  });
+  it("not required", () => {
+    expect(isRequirementFulfilled({}, "password")).toBeTruthy;
+  });
+});
+
+describe("areRequirementsFulfilled", () => {
+  it("yes", () => {
+    expect(
+      areRequirementsFulfilled({
+        name: "Zain Fathoni",
+        email: "zainfathoni@ojekbelanja.id",
+        phone: "081234567890",
+        city: "Bandung",
+        address: "Jalan jalan"
+      })
+    ).toBe(true);
+  });
+  it("no", () => {
+    expect(
+      areRequirementsFulfilled({
+        name: "Zain Fathoni",
+        email: "zainfathoni@ojekbelanja.id",
+        phone: "081234567890",
+        city: "Bandung"
+      })
+    ).toBe(false);
   });
 });
